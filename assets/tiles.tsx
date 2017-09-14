47344c25db10edba43f2e2047c403e0d75398af1
naziafakhruddin <naziafakhruddin@gmail.com>
2017-09-14 18:14:58 +0100

    add it

---
+++ assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+ <image source="2.png" width="128" height="128"/>
+</tileset>
